120c6a355b41781f17b882e0d48943b0186dd85c
Christian Augustyn <chrisaugu61@gmail.com>
2024-10-07 10:14:46 +1000

    running test with incorrect input

--- tests/add.test.js
+++ tests/add.test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 test('synchronous passing test', (t) => {
   // This test passes because it does not throw an exception.
-  assert.strictEqual(1, 1);
+  assert.strictEqual(1, 2);
 });
 
 // test('synchronous failing test', (t) => {
