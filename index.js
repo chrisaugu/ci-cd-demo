8e5ec3297a4ae264662181f3fbf720163d7a595a
Christian Augustyn <chrisaugu61@gmail.com>
2025-04-13 19:50:35 +1000

    added test, main, deploy actions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const _ = require('underscore');
+const _ = require("underscore");
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 // let result = sum(2, 3)
@@ -10,10 +10,19 @@ function sum(a, b) {
 exports.sum = sum;
 
 function add(numbers) {
+  if (_.isArray(numbers)) {
+    return numbers.reduce((a, b) => a + b);
+  } else {
     return numbers
-        .split(',')
-        .map(x => parseInt(x))
-        .reduce((a, b) => a + b)
+      .split(",")
+      .map((x) => parseInt(x))
+      .reduce((a, b) => a + b);
+  }
 }
-
 exports.add = add;
+
+function divideByZero(number) {
+  const result = number / 0;
+  return result;
+}
+exports.divideByZero = divideByZero;
--- tests/add.test.js
+++ tests/add.test.js
@@ -1,32 +1,33 @@
-const {test, describe, it} = require('node:test');
+const { test, describe, it } = require("node:test");
 // Import the necessary modules and dependencies
-const assert = require('assert');
+const assert = require("assert");
+const { add, divideByZero } = require("..");
 
-test('synchronous passing test', (t) => {
+test("synchronous passing test", (t) => {
+  let result = add([1, 2]);
   // This test passes because it does not throw an exception.
-  assert.strictEqual(1, 2);
+  assert.strictEqual(result, 3);
 });
 
-// test('synchronous failing test', (t) => {
-//   // This test fails because it throws an exception.
-//   assert.strictEqual(1, 2);
-// });
+test("synchronous failing test", (t) => {
+  let result = add("1,2");
+  // This test fails because it throws an exception.
+  assert.strictEqual(result, 3);
+});
 
 // Describe a test suite using Mocha's "describe" function
-describe('Math operations', () => {
+describe("Math operations", () => {
   // Define a test case using Mocha's "it" function
-  it('should add two numbers correctly', () => {
+  it("should add two numbers correctly", () => {
     const result = 1 + 2;
     assert.strictEqual(result, 3); // Assert the expected result
   });
-  it('should multiply two numbers correctly', () => {
+  it("should multiply two numbers correctly", () => {
     const result = 3 * 4;
     assert.strictEqual(result, 12);
   });
   // it('should handle division by zero', () => {
-  //   const divideByZero = () => {
-  //     const result = 10 / 0;
-  //   };
-  //   assert.throws(divideByZero, Error); // Assert that an error is thrown
+  //   let result = divideByZero(10)
+  //   assert.throws(result, Error); // Assert that an error is thrown
   // });
 });
